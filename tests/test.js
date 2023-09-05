5b73f5e0a891acdf9fcf5b0c58ba7eee28d008b1
Richardson Qiu <richardsonqiu@gmail.com>
2023-09-05 23:52:08 +0800

    Commented tests

--- tests/test.js
+++ tests/test.js
@@ -6,19 +6,19 @@ const app = require('../app.js');
 
 const request = supertest(app);
 
-describe('Tests app', function() {
-  it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-        done(err);
-    });
-  });
-  it('verifies post', function(done) {
-    request.post('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
-        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-        done(err);
-    });
-  });
-});
+// describe('Tests app', function() {
+//   it('verifies get', function(done) {
+//     request.get('/').expect(200).end(function(err, result) {
+//         test.string(result.body.Output).contains('Hello');
+//         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+//         done(err);
+//     });
+//   });
+//   it('verifies post', function(done) {
+//     request.post('/').expect(200).end(function(err, result) {
+//         test.string(result.body.Output).contains('Hello');
+//         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+//         done(err);
+//     });
+//   });
+// });
