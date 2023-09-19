eb18a909b304f1ab186e8cb26538b41a2454e15e
richardsonqiu <richardsonqiu@gmail.com>
2023-09-20 00:24:59 +0800

    Added logger to debug

--- functions/task/get-search-tasks.js
+++ functions/task/get-search-tasks.js
@@ -29,12 +29,16 @@ module.exports.handler = (event, context, callback) => {
         }
         
         const tasks = result.Items;
+        console.log("Before todo filtering: " + tasks);
         tasks.forEach((task) => {
             if (task.todos && Array.isArray(task.todos)) {
+                console.log("current todos: " + task.todos);
                 task.todos = task.todos.filter((todo) => todo.title.includes(keyword))
+                console.log("updated todos: ", task.todos);
             }
         })
 
+        console.log("After todo filtering: " + tasks);
         const response = {
             statusCode: 200,
             body: JSON.stringify(tasks)
