bfa24f91fea413b9aa30532b84aaa7073e221c60
richardsonqiu <richardsonqiu@gmail.com>
2023-10-08 13:25:57 +0800

    Update update todo

--- functions/todo/update-todo.js
+++ functions/todo/update-todo.js
@@ -7,13 +7,13 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.handler = (event, context, callback) => {
   const timestamp = new Date().toISOString();
   const data = JSON.parse(event.body);
-  const taskId = event.pathParameters.taskId;
 
   const params = {
     TableName: 'Todo',
+    IndexName: "task_id-create_date-index",
     Key: {
       id: event.pathParameters.todoId,
-      task_id: taskId
+      task_id: event.pathParameters.taskId,
     },
     ExpressionAttributeValues: {
       ':title': data.title,
@@ -30,14 +30,38 @@ module.exports.handler = (event, context, callback) => {
       ':updated_date': timestamp
     },
     UpdateExpression: 'SET title = :title, is_done = :is_done, due_date = :due_date, assign_username = :assign_username, assign_email = :assign_email, assign_name = :assign_name, description = :description, priority = :priority, tag1 = :tag1, tag2 = :tag2, tag3 = :tag3, updated_date = :updated_date',
-    ReturnValues: 'ALL_NEW',
-  };
+    ReturnValues: 'ALL_NEW'
+};
+
+  // const params = {
+  //   TableName: 'Todo',
+  //   Key: {
+  //     id: event.pathParameters.todoId,
+  //     task_id: taskId
+  //   },
+  //   ExpressionAttributeValues: {
+  //     ':title': data.title,
+  //     ':is_done': data.is_done,
+  //     ':due_date': data?.due_date || "",
+  //     ':assign_username': data?.assign_username || "",
+  //     ':assign_email': data?.assign_email || "",
+  //     ':assign_name': data?.assign_name || "",
+  //     ':description': data?.description || "",
+  //     ':priority': data?.priority || "",
+  //     ':tag1': data?.tag1 || "",
+  //     ':tag2': data?.tag2 || "",
+  //     ':tag3': data?.tag3 || "",
+  //     ':updated_date': timestamp
+  //   },
+  //   UpdateExpression: 'SET title = :title, is_done = :is_done, due_date = :due_date, assign_username = :assign_username, assign_email = :assign_email, assign_name = :assign_name, description = :description, priority = :priority, tag1 = :tag1, tag2 = :tag2, tag3 = :tag3, updated_date = :updated_date',
+  //   ReturnValues: 'ALL_NEW',
+  // };
 
   // update the todo in the database
   dynamoDb.update(params, (error, result) => {
     // handle potential errors
     if (error) {
-      console.error(error);
+      console.error(JSON.stringify(`error: ${error}`));
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
@@ -45,6 +69,8 @@ module.exports.handler = (event, context, callback) => {
       });
       return;
     }
+
+    console.log(JSON.stringify(`result: ${result}`));
 
     // create a response
     const response = {
